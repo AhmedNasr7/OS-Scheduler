93ea793be4a63c0be148687bfb307a1bd18bd2da
ahmed.nasr9677@gmail.com <ahmed.nasr9677@gmail.com>
2019-03-24 21:22:41 +0200

    add preemptive choose functionality

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from PyQt5.uic import loadUiType
 import sys
 from os import path
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, \
-    QPushButton
+    QPushButton, QCheckBox
 from PyQt5.QtGui import QIcon
 
 
@@ -17,6 +18,8 @@ class MainApp(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.setup_Ui()
         self.init_Buttons()
+        #self.preemptiveCheckBox = QCheckBox(' Preemptive ', self)
+
 
     def setup_Ui(self):
         '''
@@ -24,12 +27,77 @@ class MainApp(QMainWindow, Ui_MainWindow):
         '''
         self.setWindowTitle("OS Scheduler")
         self.setFixedSize(900,600)
+        #self.vlayout = QVBoxLayout()
+        self.algorithmsMenu.addItem("  First Come First Serve")
+        self.algorithmsMenu.addItem("  Shortest Job First")
+        self.algorithmsMenu.addItem("  Priority")
+        self.algorithmsMenu.addItem("  Round Robin")
+        self.algorithmsMenu.activated[str].connect(self.onActivation)
+        #self.vlayout.addWidget(self.algorithmsMenu)
+        #self.setLayout(self.vlayout)
+
+        
+
+
+        
+
 
     def init_Buttons(self):
         '''
         Buttons initializations goes here
         '''
         pass
+
+
+    def onActivation(self, algo):
+        # on picking an algorithm from the menu
+
+        if (algo == '  Priority'): # Dynamically showing preemptive/Non Preemptive checkboxes
+            self.preemptiveCheckBox = QCheckBox(' Preemptive ', self)
+            self.nonPreemptiveCheckBox = QCheckBox(' Non Preemptive ', self)
+            self.layout().addWidget(self.preemptiveCheckBox)
+            self.layout().addWidget(self.nonPreemptiveCheckBox)
+            self.preemptiveCheckBox.move(310, 90)
+            self.nonPreemptiveCheckBox.move(310, 120)
+            self.preemptiveCheckBox.resize(350, 40)
+            self.nonPreemptiveCheckBox.resize(350, 40)
+
+            self.preemptiveCheckBox.stateChanged.connect(self.preemptive_stateChanged)
+            self.nonPreemptiveCheckBox.stateChanged.connect(self.nonPreemptive_stateChanged)
+        else:
+            if self.preemptiveCheckBox is not None:
+                self.preemptiveCheckBox.deleteLater()
+                self.nonPreemptiveCheckBox.deleteLater()
+            else:
+                pass
+            
+
+
+
+
+
+    
+    def preemptive_stateChanged(self, state):
+        
+        if self.preemptiveCheckBox.isChecked():
+            self.nonPreemptiveCheckBox.setChecked(0)
+            # do other stuff
+        else:
+            self.nonPreemptiveCheckBox.setChecked(1)
+       
+
+    def nonPreemptive_stateChanged(self, state):
+        if self.nonPreemptiveCheckBox.isChecked():
+            self.preemptiveCheckBox.setChecked(0)
+            # do other stuff
+        else:
+            self.preemptiveCheckBox.setChecked(1)
+
+        
+
+
+            
+            
 
 def main():
     app = QApplication(sys.argv)
