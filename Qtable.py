ff309b9e5ab79b619771038aaadcf33042e5f668
ahmed.nasr9677@gmail.com <ahmed.nasr9677@gmail.com>
2019-03-25 02:01:03 +0200

    finish processes table UI

--- Qtable.py
+++ Qtable.py
@@ -16,8 +16,15 @@ class Table(QMainWindow, Ui_MainWindow):
         super(Table, self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.columns_count = 3
+        self.rows_count = 5
+        self.table_width = 600
+        self.table_height = self.rows_count * 34
         self.setup_Ui()
         self.init_Button()
+    
+       
+
 
 
     def setup_Ui(self):
@@ -28,15 +35,22 @@ class Table(QMainWindow, Ui_MainWindow):
         self.setWindowTitle("OS Scheduler")
         self.setFixedSize(800,600)
         self.table.move(0, 0)
-        self.table.setFixedSize(600, 400)
-        self.table.setRowCount(10)
-        self.table.setColumnCount(3)
+        self.table.setFixedSize(self.table_width, self.table_height)
+        self.table.setRowCount(self.rows_count)
+        self.table.setColumnCount(self.columns_count)
+        for i in range(self.columns_count):
+             self.table.setColumnWidth(i, self.table_width/3 - 10)
+        
         self.enterButton = QPushButton('Enter Processes', self)
         self.enterButton.resize(150, 40)
         self.enterButton.move(620, 50)
+        self.add_newRow = QPushButton("Add New Row", self)
+        self.add_newRow.resize(150, 40)
+        self.add_newRow.move(620, 100)
         self.cancelButton = QPushButton('Cancel', self)
         self.cancelButton.resize(150, 40)
-        self.cancelButton.move(620, 100)
+        self.cancelButton.move(620, 150)
+        
 
         #self.table.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
         #self.table.resizeColumnsToContents
@@ -45,17 +59,25 @@ class Table(QMainWindow, Ui_MainWindow):
 
     def init_Button(self):
         self.enterButton.clicked.connect(self.onClick_enterButton)
+        self.add_newRow.clicked.connect(self.addRow)
         self.cancelButton.clicked.connect(self.onClick_cancelButton)
 
 
 
-
     def onClick_enterButton(self):
-        print("enter clicked")
+        pass
+
+    def addRow(self):
+        self.rows_count += 1
+        if self.table_height < 34 * self.rows_count:
+            self.table_height += 34
+            
+        self.table.setFixedSize(self.table_width, self.table_height)
+        self.table.setRowCount(self.rows_count)
 
     
     def onClick_cancelButton(self):
-        print('cancel Clicked')
+        pass
 
 
 
@@ -69,8 +91,6 @@ class Table(QMainWindow, Ui_MainWindow):
         print("World")
     '''
         
-        
-            
 
 def main():
     app = QApplication(sys.argv)
